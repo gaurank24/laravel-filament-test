c2306fab2aa79acd1f67d61ba92aab491594070c
gaurank24 <gaurank.phppoets@gmail.com>
2025-06-20 18:02:33 +0530

    first commit

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    //  use HasFactory;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
+
+    protected $fillable = ['name', 'description'];
+}
